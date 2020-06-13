526d1d4973da4fd8adebca5b456a23f8328bd36f
Sian Culligan <SianCulligan@gmail.com>
2020-06-12 21:16:44 -0700

    scaffolding files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+  
+import React from 'react';
+
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Counter from './components/Counter.js';
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <Counter />
+                <Footer />
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
